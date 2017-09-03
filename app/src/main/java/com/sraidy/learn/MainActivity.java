2d1e8c24cbeaa338fe3a404fa7b887aa28378036
sraidytech <31393867+sraidytech@users.noreply.github.com>
2017-09-03 16:51:20 +0100

    Initial commit

---
+++ app/src/main/java/com/sraidy/learn/MainActivity.java
@@ -0,0 +1,26 @@
+package com.sraidy.learn;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+}
